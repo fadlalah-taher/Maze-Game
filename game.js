0cec884ca2222070b5de2cc0ef053a43a4cfbdc9
fadlalah-taher <fadeltaher111@gmail.com>
2022-05-23 11:24:55 +0300

    Adding function(CountupTimer) and edit the result score

--- game.js
+++ game.js
@@ -1,19 +1,51 @@
 window.onload = function(){
 
     // Function
-
+    var startt;
     function gameStart(){
         active = true;
+        //startt = new Date().getTime();
+        //startt = Date.now();
+        //clearInterval(timerVariable);
+        //console.log(startt);
+        var timerVariable = setInterval(countUpTimer, 1000);
         changeBackgroundColor("#eeeeee");
         status.innerHTML = "S   >   E ";
     }
 
+    function lastScore(value){
+        if(value > 0){
+            last.innerHTML = "Last " + value ;
+        }   
+    }
+    //var timerVariable = setInterval(countUpTimer, 1000);
+    var totalSeconds = 0;
+    function countUpTimer() {
+        ++totalSeconds;
+        var hour = Math.floor(totalSeconds / 3600);
+        var minute = Math.floor((totalSeconds - hour * 3600) / 60);
+        var seconds = totalSeconds - (hour * 3600 + minute * 60);
+        
+        live.innerHTML =  minute + ":" + seconds;
+      }
+    var millis;
     function gameEnd(){
         if(active){
             active = false;
             score += 5;
             won = true;
             displayResult(won);
+            console.log(startt);
+            var milliseconds = Date.now() - startt;
+            console.log("mill"+ milliseconds)
+            console.log('Seconds passed = ' + milliseconds / 1000);
+            millis = milliseconds / 1000;
+            lastScore(millis);
+            /*var end = new Date().getTime();
+            var time = end - startt;
+            console.log(startt);
+            console.log(end);
+            console.log("time"+ time);*/
         }
     }
     // Restart
@@ -50,11 +82,12 @@ window.onload = function(){
 
     function displayResult(won){
         if(won){
-            status.innerHTML = "You Won! Your Score is " + score ;
+            status.innerHTML = "You Won!" + score ;
+            yourScore.innerHTML = "Your Score " + score ;
         }
         else{
-            status.innerHTML = "You Lost! Your Score is " + score ;
-            boundaryExample.innerHTML = score;
+            status.innerHTML = "You Lost!" + score ;
+            yourScore.innerHTML = "Your Score " + score ;
         }
     }
 
@@ -65,6 +98,10 @@ window.onload = function(){
     var end = document.getElementById("end");
     var status = document.getElementById("status");
     var game = document.getElementById("game");
+    var last = document.getElementById("last");
+    var best = document.getElementById("best");
+    var yourScore = document.getElementById("score");
+    var live = document.getElementById("live");
     //var boundaryExample = document.getElementsByClassName("example");
     
 
@@ -98,3 +135,12 @@ window.onload = function(){
         cheating();
     });
 };
+/*var start = new Date().getTime();
+
+for (i = 0; i < 50000; ++i) {
+// do something
+}
+
+var end = new Date().getTime();
+var time = end - start;
+alert('Execution time: ' + time); */
